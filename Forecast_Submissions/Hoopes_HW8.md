c01b110c3193501260062a4a65303b4c98ec0676
Andrew Hoopes <cahoopes@email.arizona.edu>
2021-10-17 18:51:38 -0700

    Hoopes HW8 Submission

---
+++ Forecast_Submissions/Hoopes_HW8.md
@@ -0,0 +1,29 @@
+Homework 8 Submission - By Andrew Hoopes
+
+Week 1 Forecast: 128 cfs
+
+Week 2 Forecast: 161 cfs
+
+1.  Similarly to last week, I made my forecast based on the current flow (last availiable observation), the climatology for the month of October using the mean and median flow (week 2 forecast period ends on Oct 31), and for week 1, the relative chance of precipitation, as a rain event can increase flow and a lack of rain can decrease it.  For week 1, the current flow is weighted equally to the mean and median climatology combined (current conditions should have a large influence) while both climatologies are weighted higher for the second week (further out, a return to the mean can be expected).  For the first week, an additional multiplier is applied based on the number of days precipitation can be reasonably expected (NWS probability >50%, accumulates day to day until threshold reached).  Zero precipitation decreases the expected flow by 10%, while 2 rainy days will increase the forecast by 10%, with an additional 10% increase for each additional rainy day.
+
+2. ![Plot of previous 30 days' flow](../data/Recent_flow_week8.png)
+
+   Plot of flow over last 30 days: gives an idea of current trends, last point used for observational value.
+
+   ![Histogram of October flow](../data/October_histogram.png)
+
+   Histogram of October flows - Introuction to the monthly climatology, majority of values within a certain range, forecasts should not be outside of it.
+
+   ![Mean October flow](../data/October_mean.png)
+
+   Plot of 1989-2021 mean October flows - Gives an idea of what flows can be reasonably expected based on previous years' conditions, forms half of the climatology used to make the forecast.
+
+   ![Median October flow](../data/October_median.png)
+
+   Plot of 1989-2021 median October flows - Similar to the mean plot, but gives additional avantage of filtering out outliers, forms other half of the climatology used to make the forecast.
+
+3. Most of the comments from my partner had to do with the easiness of my code to understand.  while the code itself was well-written, the comments and the function description needed cleaning up to be more easily understandable.  A big point of concern came from how to set the precipitation index.  I myself realized beforehand that part might be confusing, and did my best to explain it, nonetheless, I improved both my comments, the function description, and the methodology and rationale behind my forecast code.  Additionally, several areas failed to follow pep8 format, I corrected each such issue to improve readability.
+
+4. Timeseries functionality was already being used, as the initial starter code used the parse dates statement to read in the datetime column.  However, I made several improvements to said functionality, mainly in cleaning up my plot of the flow over the previous 30 days.  First, once all the columns were created, I set the dataframe index to datetime, making working with and plotting datetime easier, as well as allowing for simpler referencing/indexing.  Next, I used the DateFormatter and DayLocater methods from matplotlib.dates, being sure to import them at the top of the code beforehand.  DateFormatter was used to change the x-axis labels on the plot to only show the month and day, as the year was always 2021 on that plot, while DayLocator was used to set the tick and label increments to 4 days.  These changes combined made the plot much more readable, as datetime labels were evenly spaced (no lnger reset with month) and no longer overlapped with each other thanks to the removal of the year, unecessary due to being a constant value.
+
+5.  The part of the code I am most proud of is older and does not use timeseries functionality, but uses Pandas Dataframes in a way that was only brought up in class this past week.  This part of the code uses the groupby Pandas method to group the data by day of the month (October data is already isolated using additional Pandas functionality) and take the mean and median of the flow values for each day.  Only 2 lines of code are needed, one for the mean an one for the median, and 2 Pandas series are output, the daily means/medians indexed by the day of the month.  Both series are then plotted.  The simplicity and shortness of these methods, and their functiionality in obtaining two key forecast parameters from a much larger dataset, are reasons for me being proud in this part of the code.
---
+++ Forecast_Submissions/Hoopes_HW8.py
@@ -0,0 +1,155 @@
+# %%
+# Import the modules we will use
+import os
+import numpy as np
+import pandas as pd
+# import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.dates import DateFormatter as df
+from matplotlib.dates import DayLocator as dl
+# import datetime
+
+# %%
+# ** MODIFY **
+# Set the file name and path to where you have stored the data
+filename = 'streamflow_week8.txt'
+filepath = os.path.join('../data/', filename)
+print(os.getcwd())
+print(filepath)
+
+# %%
+# Read the data into a pandas dataframe
+data = pd.read_table(filepath, sep='\t', skiprows=30,
+                     names=['agency_cd', 'site_no', 'datetime', 'flow',
+                            'code'], parse_dates=['datetime'])
+
+# Expand the dates to year month day, set index as base datetime
+data['year'] = pd.DatetimeIndex(data['datetime']).year
+data['month'] = pd.DatetimeIndex(data['datetime']).month
+data['day'] = pd.DatetimeIndex(data['datetime']).day
+data['dayofweek'] = pd.DatetimeIndex(data['datetime']).dayofweek
+data = data.set_index('datetime')
+
+# %%
+# Time series of flow values with the x axis range limited
+# Adjust this plot, make it look at the most recent month
+# New for week 8 - clean up datetime format on x-axis
+recent_data = data.tail(30)
+xformat = df("%m-%d")
+plt.style.use('seaborn-whitegrid')
+fig, ax = plt.subplots()
+ax.plot(recent_data['flow'], color='steelblue', label='Last Month')
+ax.xaxis.set_major_formatter(xformat)
+ax.xaxis.set_major_locator(dl(interval=4))
+ax.set(title="Observed Flow", xlabel="Date (Year is 2021)",
+       ylabel="Weekly Avg Flow [cfs]")
+ax.legend()
+plt.show()
+fig.savefig('Recent_flow_week8.png')
+
+# %%
+# Histogram plot of October flow
+oct_data = data[data['month'] == 10]
+my_bins = np.linspace(0, 800, num=30)
+fig, ax = plt.subplots()
+plot_title = 'October Streamflow Histogram'
+ax.hist(oct_data['flow'], bins=my_bins,
+        edgecolor='lightgrey', color='gold')
+ax.set(xlabel='October Flow cfs', ylabel='count', title=plot_title)
+plt.show()
+fig.savefig('October_histogram.png')
+
+# %%
+# Figure out how the flow behaves in October
+# - find the mean flow for each day of the month
+oct_mean = oct_data.groupby('day')['flow'].mean()
+fig, ax = plt.subplots()
+ax.plot(oct_mean.index, oct_mean.values, color='crimson', label='October mean')
+ax.set(title="Mean October Flow", xlabel="Day of Month",
+       ylabel="October Mean Flow [cfs]")
+ax.legend()
+plt.show()
+fig.savefig('October_mean.png')
+
+# %%
+# Also consider median flow
+oct_median = oct_data.groupby('day')['flow'].median()
+fig, ax = plt.subplots()
+ax.plot(oct_median.index, oct_median.values, color='crimson',
+        label='October median')
+ax.set(title="Median October Flow", xlabel="Day of Month",
+       ylabel="October Median Flow [cfs]")
+ax.legend()
+plt.show()
+fig.savefig('October_median.png')
+
+# %%
+# Create and run a function which forecasts the streamflow for the next two
+# weeks based on current flow and monthly climatology (mean and median flows)
+#
+# For week 1, also include whether significant precip is expected, set based
+# on number of days where chance of precipitation is >=50% (NWS forecast)
+
+# Link: https://forecast.weather.gov/MapClick.php?lon=-111.85758589999749&lat=34.56146253137916#.YV4DsCVlDYs
+
+# Cumulative, so 2 days with 30% chance equal one day of 50% chance
+
+# Day, night probabilities separate,
+# use whichever value is higher to represent the day
+
+# Example: for 3 30% days, set precip index to 1
+# A 4th day would increase the index to 2
+
+# Do not include for week 2, too far out to reliably predict significant events
+
+# Instead, weight climatology higher for second week,
+# as opposed to equal weights of first week
+
+# Equal weight means mean and median combined are equal to last observation
+
+
+def flowcast(obs, mean, med, a, day):
+    """ This function takes the last streamflow observation and interpolates
+    streamflow for the next two weeks using the climatological
+    mean and median flow to weight the interpolation.
+
+    For the first week, additional weighting comes from precipitation,
+    while climatology is weighted higher during the second week.
+
+    Inputs: obs - last streamflow observation in the initial PANDAS Dataframe
+    mean - mean flow for the month of the forecast (October)
+    med - median flow for the month of the forecast (October)
+    a - number of days where precipitation is expected,
+    set based on combined NWS daily precipitation probabilities divided by 50
+    day - day of the month of the Monday at the start of the forecast period
+
+    Outputs: 2 forecast values (f1 and f2),
+    f1 for the first week, and f2 for the second week"""
+
+    week1 = np.zeros(7)
+    week2 = np.zeros(7)
+    for i in range(7):
+        week1[i] = mean.values[day+i]*0.25+med.values[day+i]*0.25+obs*0.5
+        week2[i] = mean.values[day+i+7]*0.4+med.values[day+i+7]*0.4+obs*0.2
+    f1 = round(np.mean(week1)*(1+(a-1)/10))
+    f2 = round(np.mean(week2))
+    return(f1, f2)
+
+
+# Define the necessary variables and call the function
+
+# Manually set precipitation index using link, explanation above
+
+# Set date based on day of month of day before first day 
+# in week 1 forecast period
+# No precip expected this week
+# Should be set to 17 for week 8.
+
+last_flow = recent_data["flow"].iloc[-1]
+precip = 0
+date = 17
+flowcast1, flowcast2 = flowcast(last_flow, oct_mean, oct_median, precip, date)
+print('Week 1 forecast:', flowcast1, 'cfs')
+print('Week 2 forecast:', flowcast2, 'cfs')
+
+# %%
